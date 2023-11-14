4978631031a02f88baf6be7c71453ebd9a460388
anchal00 <anchal.sharma@ridecell.com>
2023-11-14 18:19:37 +0530

    fix format

--- cli/optimus.py
+++ cli/optimus.py
@@ -13,13 +13,13 @@ def main(argv):
     arg_parser.add_argument("-p", metavar="PORT", type=int, default=53,
                             help="Port to run the server on (defaults to 53)")
     arg_parser.add_argument("-t", metavar="THREADS", type=int, default=100,
-                            help="Number of worker threads to spin up for handling requests (defaults to 100)"
-    )
+                            help="Number of worker threads to spin up for handling requests (defaults to 100)")
     args = arg_parser.parse_args(argv)
     if args.r:
         run_server(args.p, args.t)
     else:
         arg_parser.print_help()
 
+
 def main_wrapper():
-    return main(sys.argv[1:])
+    main(sys.argv[1:])
