188de7bf4d48df6c2210ceddb86aeef1421e71de
anchal00 <anchal.sharma@ridecell.com>
2023-11-14 18:13:39 +0530

    fix

--- optimus.py
+++ optimus.py
@@ -11,8 +11,7 @@ arg_parser.add_argument("-r", action="store_true", help="Run DNS server")
 arg_parser.add_argument("-p", metavar="PORT", type=int, default=53,
                         help="Port to run the server on (defaults to 53)")
 arg_parser.add_argument("-t", metavar="THREADS", type=int, default=100,
-                        help="Number of worker threads to spin up for handling requests (defaults to 100)"
-)
+                        help="Number of worker threads to spin up for handling requests (defaults to 100)")
 
 if __name__ == '__main__':
     args = arg_parser.parse_args()
