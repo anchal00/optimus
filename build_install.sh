bed2cedb02d0c22d0adbf6100fe26bed4b6e6831
anchal00 <anchal82199@gmail.com>
2024-02-19 13:53:37 +0530

    Remove script

--- build_install.sh
+++
@@ -1,6 +0,0 @@
-# !/usr/bin/bash
-echo -e "Packaging optimus :) ...\n\n"
-python -m build .
-echo -e "Installing optimus...\n\n"
-pip install dist/optimus-1.0.tar.gz
-echo "optimus installed successfully..."
