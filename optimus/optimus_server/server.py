2832798580342c2afcb4ccd212a5bc0358b9dbc6
anchal00 <anchal82199@gmail.com>
2024-08-20 15:06:09 +0530

    1. Gracefully shutdown the server upon keyboard interrupts 2. Add wrapper script as a workaround to allow binding optimus on port 53

--- optimus/optimus_server/server.py
+++ optimus/optimus_server/server.py
@@ -9,8 +9,13 @@ def run_server(port: int, worker_threads: int):
     master_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     master_socket.bind(("0.0.0.0", port))
     log(f"Started Optimus Server on Port {port}")
-    # TODO: Test with ProcessPoolExecutor and EPOLL
-    with futures.ThreadPoolExecutor(max_workers=worker_threads) as pool:
-        while True:
-            received_bytes, address = master_socket.recvfrom(600)
-            pool.submit(handle_request, master_socket, received_bytes, address)
+    # TODO: Test with Process PoolExecutor and EPOLL
+    try:
+        with futures.ThreadPoolExecutor(max_workers=worker_threads) as pool:
+            while True:
+                received_bytes, address = master_socket.recvfrom(600)
+                pool.submit(handle_request, master_socket, received_bytes, address)
+    except KeyboardInterrupt:
+        log("Goodbye ! Shutting Down the server...")
+    finally:
+        master_socket.close()
